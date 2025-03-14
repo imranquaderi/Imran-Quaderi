cb2abf1cdcc42f659c2c4744e59ba54ea750ce83
imranquaderi <imranquaderi@gmail.com>
2025-03-14 20:20:37 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -16,10 +16,12 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.sql.Driver;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
+
 
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
@@ -35,78 +37,47 @@ public class TestCases {
      */
     @Test
     public void testCase01() throws InterruptedException{
-        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
-        Thread.sleep(2000);
-        
-        WebElement nameField = driver.findElement(By.xpath("//input[@aria-labelledby='i1 i4']"));
-        nameField.sendKeys("Crio Learner");
+
+        WebElement nameField = driver.findElement(By.xpath("(//input[contains(@class,'zHQkBf')])[1]"));
+        Wrappers.enterText(nameField,"Crio Learner");
         Thread.sleep(2000);
         
 
-        WebElement YAYP = driver.findElement(By.xpath("//textarea[@aria-describedby='i7 i8']"));
+        WebElement YAYP = driver.findElement(By.xpath("//textarea[contains(@class,'KHxj8b tL9Q4c')]"));
         long epochTime = System.currentTimeMillis() / 1000; // Get epoch time in seconds
         String response = "I want to be the best QA Engineer! " + epochTime;
         YAYP.sendKeys(response);
         Thread.sleep(2000);
 
-        WebElement ATExp = driver.findElement(By.xpath("//span[text()='0 - 2']"));
-        ATExp.click();
+        Wrappers.radioButton(driver,"0 - 2");
         Thread.sleep(2000);
 
-        WebElement learnJava =driver.findElement(By.xpath("//span[text()='Java']"));
-        learnJava.click();
-
-        WebElement learnSelenium = driver.findElement(By.xpath("//span[text()='Selenium']"));
-        learnSelenium.click();
-
-        WebElement learnTestNG = driver.findElement(By.xpath("//span[text()='TestNG']"));
-        learnTestNG.click();
-
+        Wrappers.checkBoxes(driver,"Java");
+        Wrappers.checkBoxes(driver, "Selenium");
+        Wrappers.checkBoxes(driver, "TestNG");
         Thread.sleep(2000);
 
-        WebElement choose = driver.findElement(By.xpath("//span[text()='Choose']"));
-        choose.click();
+        Wrappers.clickDropDown(driver);
         Thread.sleep(2000);
 
-        WebElement Mr = driver.findElement(By.xpath("(//span[text()='Mr'])[2]"));
-        Mr.click();
+        Wrappers.selectTitle(driver,"Mr");
         Thread.sleep(2000);
 
-        // Calculate the date 7 days ago
-        LocalDate sevenDaysAgo = LocalDate.now().minusDays(7);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formattedDate = sevenDaysAgo.format(formatter);
-        System.out.println(formattedDate);
-
-        // Locate the date input field and enter the calculated date
-        WebElement dateField = driver.findElement(By.xpath("//input[@type='date']"));
-        dateField.sendKeys(formattedDate);
+        Wrappers.date(driver);
         Thread.sleep(2000);
 
 
-        Actions action = new Actions(driver);
-        action.
-        keyDown(Keys.TAB).
-        keyUp(Keys.TAB).
-        keyDown(Keys.TAB).
-        keyUp(Keys.TAB).
-        sendKeys("7").
-        keyDown(Keys.TAB).
-        keyUp(Keys.TAB).
-        sendKeys("30").
-        perform();
+        WebElement hourField = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+        hourField.sendKeys("7");
 
-        // WebElement hourField = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
-        // hourField.sendKeys("7");
-
-        // WebElement minuteField = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
-        // minuteField.sendKeys("30");
+        WebElement minuteField = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        minuteField.sendKeys("30");
 
         WebElement submitButton = driver.findElement(By.xpath("//span[contains(text(),'Submit')]"));
         submitButton.click();
         Thread.sleep(2000);
 
-        WebElement sussessMessage = driver.findElement(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']"));
+        WebElement sussessMessage = driver.findElement(By.xpath("//div[@class='vHW8K']"));
         System.out.println(sussessMessage.getText());
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
@@ -126,7 +97,7 @@ public class TestCases {
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
+    public void startBrowser() throws InterruptedException
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
@@ -146,6 +117,9 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        Thread.sleep(2000);
     }
 
     
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,11 +1,14 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +19,103 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void enterText(WebElement element, String text){
+        try{
+
+            element.clear();
+            element.sendKeys(text);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+     }
+
+
+     public static void radioButton(ChromeDriver driver, String value){
+        try{
+
+            List <WebElement> radioButtons = driver.findElements(By.xpath("//div[contains(@class,'zwllIb')]"));
+            for(WebElement radioButton: radioButtons){
+                if(radioButton.getText().equals(value)){
+                    radioButton.click();
+                }
+                break;
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+     }
+
+     public static void checkBoxes(ChromeDriver driver, String dropdowntext){
+        try{
+          List <WebElement> checkBoxes = driver.findElements(By.xpath("//label[contains(@class,'Yri8Nb')]"));
+          for(WebElement checkBox:checkBoxes){
+            if(checkBox.getText().equals(dropdowntext)){
+                if(!checkBox.isSelected()){
+                    checkBox.click();
+                }
+                break;
+            }
+          }
+        }
+
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+     }
+
+     public static void clickDropDown(ChromeDriver driver){
+         WebElement titleDropDown= driver.findElement(By.xpath("//div[contains(@class,'KKjvXb')]"));
+         titleDropDown.click();
+
+    }
+
+    public static void selectTitle(ChromeDriver driver, String abv){
+        List<WebElement> titles = driver.findElements(By.xpath("//div[contains(@class,'ncFHed')]//div[contains(@class,'OIC90c')]"));
+        for(WebElement title :titles){
+            if(title.getText().equals(abv)){
+                title.click();
+            }
+            break;
+        }
+    }
+
+
+    public static void date(ChromeDriver driver){
+        try{
+            WebElement dateField = driver.findElement(By.xpath("//input[@type='date']"));
+            LocalDate sevenDaysAgo = LocalDate.now().minusDays(7);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            String formattedDate = sevenDaysAgo.format(formatter);
+            System.out.println(formattedDate);
+            dateField.sendKeys(formattedDate);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+       
+
+    }
+
+
+public static void clickonElement(ChromeDriver driver, WebElement element){
+    try{
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", element);
+
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
 }
+
+
+}
+
+
+
+
